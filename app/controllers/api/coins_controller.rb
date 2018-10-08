c320c5d29d4996f1c083eb4c6e8a55f1f924be59
tchoutdara <monkeymilitia444@gmail.com>
2018-10-08 10:07:53 -0600

    add more to coin controller

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -10,6 +10,14 @@ class Api::CoinsController < ApplicationController
     cmc_id = params[:coin].upcase
     listings = HTTParty.get("#{BASE_URL}listings")
     listing = listings['data'].find { |l| l['symbol'] == cmc_id }
+    res = HTTParty.get("{BASE_URl}/ticker/#{listing['id']}")
+    if coin = Coin.create_by_cmc_id(res)
+      watched = WatchedCoin.find_or_create_by(coin_id: coin.id, user_id: current_user.id)
+      watched.update(initial_price: coin.price) if watched.initial_price.nil?
+      render json: coin
+    else
+      render json: { errors: 'Coin Not Found'}, status: 422
+    end
   end
 
   def show
