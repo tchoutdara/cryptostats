5809901c6eadcfa992388cd05d6beb9ef08d5da6
tchoutdara <monkeymilitia444@gmail.com>
2018-10-08 10:03:38 -0600

    set up coin controller

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -1,17 +1,31 @@
 class Api::CoinsController < ApplicationController
+  before_action :set_coin, only: [:show, :update, :destroy]
+  BASE_URL = 'https://api.coinmarketcap.com/v2/'
+
   def index
-    
+    render json: current_user.coins
   end
 
   def create
+    cmc_id = params[:coin].upcase
+    listings = HTTParty.get("#{BASE_URL}listings")
+    listing = listings['data'].find { |l| l['symbol'] == cmc_id }
   end
 
   def show
+    render json: @coin
   end
 
   def update
+    current_user.watched_coins.find_by(coin_id: @coin.id).destroy
   end
 
   def destroy
+    @coin.destroy
   end
+
+  private
+    def set_coin
+      @coin = Coin.find(params[:id])
+    end
 end
